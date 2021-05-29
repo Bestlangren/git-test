98b56699069c2adc546ee1161c69b8c9961d5792
wenwneaier <845906996@qq.com>
2021-05-29 21:40:52 +0800

    commit

---
+++ src/main/java/com/wql/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.wql.git;
+
+/**
+ * Description
+ * User:
+ * Date:
+ * Time:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
