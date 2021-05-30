2d16806d75a39580ce67126664fd5602d80ff58c
wenwneaier <845906996@qq.com>
2021-05-30 16:54:30 +0800

    master sixth commit

--- src/main/java/com/wql/git/GitTest.java
+++ src/main/java/com/wql/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("secondBranch test");
+        System.out.println("master push");
     }
 }
